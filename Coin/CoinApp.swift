40340864d056b60f9842ef60057a354353a8fc1d
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-05-14 23:51:13 +0900

    Initial Commit

---
+++ Coin/CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinApp.swift
+//  Coin
+//
+//  Created by 조유진 on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coin
+//
+//  Created by 조유진 on 5/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
